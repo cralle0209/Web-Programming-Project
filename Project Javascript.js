b80e3df74543a740834a7e853030f3faea2a45ee
cralle0209 <42586061+cralle0209@users.noreply.github.com>
2021-04-26 20:37:22 -0400

    Update Project Javascript.js

--- Project Javascript.js
+++ Project Javascript.js
@@ -14,22 +14,22 @@ function scene() {
 }
 </script>
 
-function encrypt(message = '', key = ''){
+function encrypt(message, key){
     let message = CryptoJS.AES.encrypt(message, key);
     return message.toString();
 }
-function decrypt(message = '', key = ''){
+function decrypt(message, key){
     let code = CryptoJS.AES.decrypt(message, key);
     let decryptedMessage = code.toString(CryptoJS.enc.Utf8);
 
     return decryptedMessage;
 }
 
-function encrypt(message = '', key = ''){
+function encrypt(message, key){
     let message = CryptoJS.DES.encrypt(message, key);
     return message.toString();
 }
-function decrypt(message = '', key = ''){
+function decrypt(message, key){
     let code = CryptoJS.DES.decrypt(message, key);
     let decryptedMessage = code.toString(CryptoJS.enc.Utf8);
 
