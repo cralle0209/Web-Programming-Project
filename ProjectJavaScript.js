bed8c9b90e85f3fb94885c437ad4f9344da0bec1
cralle0209 <42586061+cralle0209@users.noreply.github.com>
2021-04-26 20:44:05 -0400

    Update ProjectJavaScript.js

--- ProjectJavaScript.js
+++ ProjectJavaScript.js
@@ -1,3 +1,19 @@
+<script>
+function update() {
+var element = document.getElementById("myprogressBar");
+var width = 1;
+var identity = setInterval(scene, 10);
+function scene() {
+	if (width >= 100) {
+	clearInterval(identity);
+	} else {
+	width++;
+	element.style.width = width + '%';
+	}
+}
+}
+</script>
+
 function encrypt(message, key){
     let message = CryptoJS.AES.encrypt(message, key);
     return message.toString();
