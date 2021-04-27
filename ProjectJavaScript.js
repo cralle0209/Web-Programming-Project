ce964ed2f03faf871aad3066cb7d4fc160c03fba
cralle0209 <42586061+cralle0209@users.noreply.github.com>
2021-04-26 20:40:52 -0400

    Add files via upload

---
+++ ProjectJavaScript.js
@@ -0,0 +1,21 @@
+function encrypt(message, key){
+    let message = CryptoJS.AES.encrypt(message, key);
+    return message.toString();
+}
+function decrypt(message, key){
+    let code = CryptoJS.AES.decrypt(message, key);
+    let decryptedMessage = code.toString(CryptoJS.enc.Utf8);
+
+    return decryptedMessage;
+}
+
+function encrypt(message, key){
+    let message = CryptoJS.DES.encrypt(message, key);
+    return message.toString();
+}
+function decrypt(message, key){
+    let code = CryptoJS.DES.decrypt(message, key);
+    let decryptedMessage = code.toString(CryptoJS.enc.Utf8);
+
+    return decryptedMessage;
+}
